6345b027d7be1fedc6b57ac4187505c2c744f680
Yusuf Arslan <yusufarsln98@outlook.com>
2023-07-20 21:13:37 +0300

    map utils remainged

--- map_ws/build/hector_slam/hector_nav_msgs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yusuf/ydlidar_ws/src/hector_slam/hector_nav_msgs
-# Build directory: /home/yusuf/ydlidar_ws/build/hector_slam/hector_nav_msgs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- map_ws/build/hector_slam/hector_slam_launch/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yusuf/ydlidar_ws/src/hector_slam/hector_slam_launch
-# Build directory: /home/yusuf/ydlidar_ws/build/hector_slam/hector_slam_launch
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
