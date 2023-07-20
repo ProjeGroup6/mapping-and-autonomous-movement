31e7f9c79455554550ff794dacfffdc594d14792
Yusuf Arslan <yusufarsln98@outlook.com>
2023-07-20 21:10:36 +0300

    autonomous movement and mapping added

---
+++ map_ws/build/hector_slam/hector_nav_msgs/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yusuf/ydlidar_ws/src/hector_slam/hector_nav_msgs
+# Build directory: /home/yusuf/ydlidar_ws/build/hector_slam/hector_nav_msgs
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
